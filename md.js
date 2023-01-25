ec4ff90fb610304277fe225d3170adb61a360e68
adudu21 <74877817+aduud21@users.noreply.github.com>
2023-01-25 23:46:19 +0000

    Made some code look a bit better

--- md.js
+++ md.js
@@ -1,15 +1,8 @@
-const { MessageEmbed } = require('discord.js');
-//44
 module.exports = async(client, messageDelete) => {
-    
-
     try {
       if (messageDelete.channel.type === 'dm') return;
         if (!require('./database/modlogs.json')[messageDelete.guild.id]) return;
         let modLogsID  = require('./database/modlogs.json')[messageDelete.guild.id].channel;
-
-        
-
         await messageDelete.guild.channels.cache.get(modLogsID).send(`
 ****Message log****
 
@@ -23,7 +16,7 @@ UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`);
               if (require('./database/modlogs.json')[messageDelete.guild.id]){
       const filelog = require('./database/modlogs.json')
   delete filelog[messageDelete.guild.id]
-  console.log("Kinda Optimized space: Somebody put modlogs for a channel then deleted that channel")
+  console.log("Kinda Optimized space: Somebody put modlogs for a channel then deleted that channel or the bot no longer has access to the channel")
               }
         
     }
