81a0dadb69dae38adef16a3ca684233749eb968b
adudu21 <74877817+aduud21@users.noreply.github.com>
2023-01-27 04:01:28 +0000

    On/off switch

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const start_up_message = true; // Possible values: true, false
+
 const { Client, ClientUser, MessageEmbed, Intents } = require('discord.js')
 console.log('⏳ -> [LOGIN] Trying to login with provided token, if this takes longer than 5 minutes it might be cause you provided a invaild token')
 const fs = require('fs');
@@ -8,8 +10,10 @@ module.exports = async(client) => {
       console.log(`☑️ -> [LOGIN] Logged into token as user ${client.user.tag}`)
 client.user.setActivity(`h!help | ${client.guilds.cache.size} servers`, { type: "LISTENING"})
   try {
+    if  (start_up_message){
 const channel = client.channels.cache.get('957439649142407248')
 channel.send(`🔃 Bot restarted 🔃`)
+  }
   } catch (error) {
 console.log("Could not send restart message in selected channel, ready.js in events folder")
     return;
