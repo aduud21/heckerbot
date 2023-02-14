86570ce664aa595a6976c275eae5252d9cad70f8
aduud21alt <125336212+aduud21alt@users.noreply.github.com>
2023-02-14 05:28:10 +0000

    t

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const start_up_message = true; // Possible values: true, false
+const start_up_message = false; // Possible values: true, false
 
 const { Client, ClientUser, MessageEmbed, Intents } = require('discord.js')
 console.log('⏳ -> [LOGIN] Trying to login with provided token, if this takes longer than 5 minutes it might be cause you provided a invaild token')
