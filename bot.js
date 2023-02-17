48c414d46c8d5800492c5950646b1a8426a58f9a
adudu21 <74877817+aduud21@users.noreply.github.com>
2023-02-17 07:54:10 +0000

    MCS (Minor Changes)

--- bot.js
+++ bot.js
@@ -41,7 +41,6 @@ client.login(encryptor.decrypt(process.env.TOKEN));
 console.log('⌛-> [LOGINDATA] Data found, program will try to use it!')
 const { keep_alive } = require('./keep_alive');
 require('./utils/defines')(client);
-require('./utils/structure/registery')(client);
 require('./utils/handlers/commands')(client);
 require('./utils/handlers/events')(client);
 require('./slashcommands/info')(client);
