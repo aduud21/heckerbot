445a25776d4f656215a43bea7fe576ddab45bebf
adudu21 <74877817+aduud21@users.noreply.github.com>
2023-02-17 03:39:43 +0000

    Handling for too long messages (For setmodlog command)

--- md.js
+++ md.js
@@ -1,6 +1,7 @@
 module.exports = async(client, messageDelete) => {
     try {
-      if (messageDelete.channel.type === 'dm') return;
+       if (messageDelete.content.length < 1826){
+          if (messageDelete.channel.type === 'dm') return;
         if (!require('./database/modlogs.json')[messageDelete.guild.id]) return;
         let modLogsID  = require('./database/modlogs.json')[messageDelete.guild.id].channel;
         await messageDelete.guild.channels.cache.get(modLogsID).send(`
@@ -9,8 +10,19 @@ module.exports = async(client, messageDelete) => {
 Message sent by <@${messageDelete.author.id}>,
 Message deleted in <#${messageDelete.channel.id}> 
 Message: ´${messageDelete.content}´
+UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`)      }
+      else{
+        if (messageDelete.channel.type === 'dm') return;
+        if (!require('./database/modlogs.json')[messageDelete.guild.id]) return;
+        let modLogsID  = require('./database/modlogs.json')[messageDelete.guild.id].channel;
+        await messageDelete.guild.channels.cache.get(modLogsID).send(`
+****Message log****
+
+Message sent by <@${messageDelete.author.id}>,
+Message deleted in <#${messageDelete.channel.id}> 
+Message: <Message is too long to show...>
 UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`);
-        
+      }
     } catch (error) {
       
               if (require('./database/modlogs.json')[messageDelete.guild.id]){
