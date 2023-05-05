6b4f820dc1cb80e96ea0f87a25668d70e0169547
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-05-05 01:11:27 +0000

    rate limit, updated quiz command and improvements

--- slashcommands/checklink.js
+++ slashcommands/checklink.js
@@ -20,10 +20,34 @@ const rankThreatLevel = (threatTypes) => {
     return "very likely malicious";
   }
 }
+const interactionCooldowns = new Map() // get userids for cooldown, should be above module.exports = async (client) => {
 module.exports = async (client) => {
   client.ws.on("INTERACTION_CREATE", async (interaction) => {
     const commandId = interaction.data.id;
     const commandName = interaction.data.name;
+    // startcooldown 
+if (commandName === 'checklink') {
+    const userId = interaction.member.user.id;
+  if (interactionCooldowns.has(userId)) {
+    const remainingCooldown = interactionCooldowns.get(userId) - Date.now();
+    if (remainingCooldown > 0) {
+      client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: `You can use this command again in ${remainingCooldown}ms.`,
+          },
+        },
+      });
+      return;
+    }
+  }
+  const cooldownTime = 5000
+  interactionCooldowns.set(userId, Date.now() + cooldownTime);
+  setTimeout(() => {
+    interactionCooldowns.delete(userId);
+  }, cooldownTime) // end of col
+}
     if (commandName == "checklink") {
       if (blacklisted.includes(interaction.member.user.id)) {
         return;
--- slashcommands/quiz.js
+++ slashcommands/quiz.js
@@ -2,36 +2,66 @@
 const REALLYMAXHEARINGREALLY1234REAL = 100
 require('events').EventEmitter.prototype._maxListeners = REALLYMAXHEARINGREALLY1234REAL;
 require('events').defaultMaxListeners = REALLYMAXHEARINGREALLY1234REAL;
-// ONLY REMOVE THE ABOVE CODE IF YOU KNOW WHAT YOUE DOING
+// ONLY REMOVE THE ABOVE CODE IF YOU KNOW WHAT YOU'RE DOING
 const { Client, ClientUser, MessageEmbed, Intents } = require('discord.js')
+const axios = require('axios');
 const { blacklisted } = require('../config/bot.json')
-module.exports = async(client) => {
-    client.ws.on("INTERACTION_CREATE", (interaction) => {
-        const commandId = interaction.data.id;
-        const commandName = interaction.data.name;
-        
-        if (commandName == "quiz") {
-          if (blacklisted.includes(interaction.member.user.id)){
-  return;
+async function getq() {
+  const apiUrl = 'https://opentdb.com/api.php?amount=1&type=boolean';
+  const response = await axios.get(apiUrl);
+  return response.data.results[0];
+}
+const interactionCooldowns = new Map() // get userids for cooldown, should be above module.exports = async (client) => {
+module.exports = async (client) => {
+  client.ws.on('INTERACTION_CREATE', async (interaction) => {
+    const commandName = interaction.data.name;
+    // startcooldown 
+if (commandName === 'quiz') {
+    const userId = interaction.member.user.id;
+  if (interactionCooldowns.has(userId)) {
+    const remainingCooldown = interactionCooldowns.get(userId) - Date.now();
+    if (remainingCooldown > 0) {
+      await client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: `You can use this command again in ${remainingCooldown}ms.`,
+          },
+        },
+      });
+      return;
+    }
+  }
+  const cooldownTime = 5000
+  interactionCooldowns.set(userId, Date.now() + cooldownTime);
+  setTimeout(() => {
+    interactionCooldowns.delete(userId);
+  }, cooldownTime) // end of col
+}
+    if (commandName === 'quiz') {
+      if (blacklisted.includes(interaction.member.user.id)) {
+        return;
+      }
+      console.log(`Slash command ${commandName} ran`)
+      const triviaQuestion = await getq()
+      const question = triviaQuestion.question;
+      let answers = [triviaQuestion.correct_answer]
+      if (answers[0] === 'True') {
+        answers[0] += 'e'
+      }
+      answers = answers.sort()
+      const noq = question.replace(/&quot;/g, '"');
+      const bruhhowmuch = noq.replace(/&#039;/g, "'");
+      const content = `
+      Random Question: ${bruhhowmuch} \n\nThink about the answer, then click this => ||${answers}|| to view the correct answer, This data is provided by [opentdb](<https://opentdb.com/>)`
+      await client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: content
           }
-          const quiz = require('../utils/structure/exports/json/quiz.json');
-    const item = quiz[Math.floor(Math.random() * quiz.length)];
-    const filter = response => {
-        return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-    };
-          if (commandName == "quiz") console.log(`Slash command ${commandName} ran`);
-            if (commandName == "quiz") client.api.interactions(interaction.id, interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content: `Random Question:
-${item.question}
-
-Think the answer, then click this => ||${item.answers}|| to view the correct answer`
- }
-       }
-        }).catch(() => {})
         }
-    });
-
-};
+      }).catch(() => {})
+    }
+  })
+}
