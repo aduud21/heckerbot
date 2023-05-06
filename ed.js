e00cb66ea31ba4bcf234cf23c1e51a4ed535e0e6
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-05-06 09:57:57 +0000

    supposed working ratelmit

--- ed.js
+++ ed.js
@@ -1,6 +1,15 @@
-const CryptoJS = require('crypto-js')
+const CryptoJS = require('crypto-js');
 const fs = require('fs');
-const key = process.env.DONOTSHARETHIS
+const async = require('async');
+const key = process.env.DONOTSHARETHIS;
+const queue = async.queue(async (task) => {
+  const { newMessage, modLogsID, content } = task;
+  try {
+    await newMessage.guild.channels.cache.get(modLogsID).send(content);
+  } catch (error) {
+    console.error(error)
+  }
+}, 1)
 module.exports = (client) => {
   const { Client, ClientUser, MessageEmbed, Intents } = require('discord.js');
   client.on('messageUpdate', async(oldMessage, newMessage) => {
@@ -10,41 +19,47 @@ module.exports = (client) => {
       var flyMessage = `${oldMessage.content}${newMessage.content}`
       if (flyMessage.length < 1816){
         if (newMessage.channel.type === 'dm') return;
-        const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
-        const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-        const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        if (!decryptedData[newMessage.guild.id]) return;
-        let modLogsID = decryptedData[newMessage.guild.id].channel;
-        await newMessage.guild.channels.cache.get(modLogsID).send(`
-****Message log****
+        const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
+      const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+     if (!decryptedData[newMessage.guild.id]) return;
+      let modLogsID = decryptedData[newMessage.guild.id].channel
+      queue.push({
+        newMessage,
+        modLogsID, 
+        content: `****Message log****
 
 Message by <@${newMessage.author.id}>,
 Message edited in <#${newMessage.channel.id}> 
 Before: ${oldMessage.content}
 After: ${newMessage.content}
-UserID: ${newMessage.author.id} | Message ID: ${newMessage.id}`)
+UserID: ${newMessage.author.id} | Message ID: ${newMessage.id}`
+                 })
       } else {
         if (newMessage.channel.type === 'dm') return;
-        const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
-        const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-        const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        if (!decryptedData[newMessage.guild.id]) return;
-        let modLogsID  = decryptedData[newMessage.guild.id].channel;
-        await newMessage.guild.channels.cache.get(modLogsID).send(`
-****Message log****
+        const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
+      const bytes = CryptoJS.AES.decrypt(ciphertext, key);
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+  if (!decryptedData[newMessage.guild.id]) return;
+      let modLogsID = decryptedData[newMessage.guild.id].channel
+      queue.push({
+        newMessage,
+        modLogsID, 
+        content: `****Message log****
 
 Message by <@${newMessage.author.id}>,
 Message edited in <#${newMessage.channel.id}> 
-Message: <Message is too long to show>
-UserID: ${newMessage.author.id} | Message ID: ${newMessage.id}`);
+<Message too long>
+UserID: ${newMessage.author.id} | Message ID: ${newMessage.id}`
+                 })
       }
     } catch (error) {
-      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
-      const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
+      const bytes = CryptoJS.AES.decrypt(ciphertext, key)
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
       if (decryptedData[newMessage.guild.id]) {
-        delete decryptedData[newMessage.guild.id];
-        console.log("Kinda Optimized space: Somebody put modlogs for a channel then deleted that channel or the bot no longer has access to the channel");
+        delete decryptedData[newMessage.guild.id]
+        console.log("Kinda Optimized space: Somebody put modlogs for a channel then deleted that channel or the bot no longer has access to the channel")
       }
     }
   })
--- md.js
+++ md.js
@@ -1,44 +1,59 @@
 const CryptoJS = require('crypto-js');
 const fs = require('fs');
-const key = process.env.DONOTSHARETHIS
+const async = require('async');
+const key = process.env.DONOTSHARETHIS;
+const queue = async.queue(async (task) => {
+  const { messageDelete, modLogsID, content } = task;
+  try {
+    await messageDelete.guild.channels.cache.get(modLogsID).send(content);
+  } catch (error) {
+    console.error(error)
+  }
+}, 1)
 module.exports = async (client, messageDelete) => {
   if (messageDelete.author.bot) return;
   try {
     if (messageDelete.content.length < 1826) {
       if (messageDelete.channel.type === 'dm') return;
-      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
+      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
       const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
       if (!decryptedData[messageDelete.guild.id]) return;
-      let modLogsID = decryptedData[messageDelete.guild.id].channel;
-      await messageDelete.guild.channels.cache.get(modLogsID).send(`
-****Message log****
+      let modLogsID = decryptedData[messageDelete.guild.id].channel
+      queue.push({
+        messageDelete,
+        modLogsID,
+        content: `****Message log****
 
 Message sent by <@${messageDelete.author.id}>,
 Message deleted in <#${messageDelete.channel.id}> 
 Message: ´${messageDelete.content}´
-UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`)
+UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`
+      })
     } else {
       if (messageDelete.channel.type === 'dm') return;
-      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
+      const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
       const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
       if (!decryptedData[messageDelete.guild.id]) return;
-      let modLogsID = decryptedData[messageDelete.guild.id].channel;
-      await messageDelete.guild.channels.cache.get(modLogsID).send(`
-****Message log****
+      let modLogsID = decryptedData[messageDelete.guild.id].channel
+      queue.push({
+        messageDelete,
+        modLogsID,
+        content: `****Message log****
 
 Message sent by <@${messageDelete.author.id}>,
 Message deleted in <#${messageDelete.channel.id}> 
 Message: <Message is too long to show>
-UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`);
+UserID: ${messageDelete.author.id} | Message ID: ${messageDelete.id}`
+      });
     }
   } catch (error) {
-    const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8');
+    const ciphertext = fs.readFileSync('./database/realmodlogs.txt', 'utf8')
     const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
     if (decryptedData[messageDelete.guild.id]) {
-      delete decryptedData[messageDelete.guild.id];
+      delete decryptedData[messageDelete.guild.id]
       console.log("Kinda Optimized space: Somebody put modlogs for a channel then deleted that channel or the bot no longer has access to the channel")
     }
   }
