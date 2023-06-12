64ee98d4ec835b61456f5f2b4e243dbd7b7f0aa3
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-06-12 09:53:24 +0000

    d

--- keep_alive.js
+++ keep_alive.js
@@ -5,5 +5,7 @@ const port = Math.floor(Math.random() * (9000 - 2000 + 1)) + 2000;
 app.use(helmet());
 app.get('/', (req, res) => res.redirect('https://discord.gg/YSEB7PnHVV'));
 app.listen(port, '0.0.0.0', () =>
-    console.log(`✅ -> Keep alive currently active, Code (bot) by adudu21 (AGuyThatLikesFurrys), listen on port: ${port}`)
+    console.log(
+        `✅ -> Keep alive currently active, Code (bot) by adudu21 (AGuyThatLikesFurrys), listen on port: ${port}`
+    )
 );
