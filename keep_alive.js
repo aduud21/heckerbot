55570778fec4b4a72c1fad65a3a736276b354e2f
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-06-12 01:44:23 +0000

    update

--- keep_alive.js
+++ keep_alive.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-const port = 3000;
+const port = Math.floor(Math.random() * (9000 - 2000 + 1)) + 2000;
 app.use(helmet());
 app.get('/', (req, res) => res.redirect('https://discord.gg/YSEB7PnHVV'));
-app.listen(port, () =>
+app.listen(port, '0.0.0.0', () =>
     console.log(`✅ -> Keep alive currently active, Code (bot) by adudu21 (AGuyThatLikesFurrys)`)
 );
