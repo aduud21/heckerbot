6609aae204515c9ee3771106414c93e69b1c9a35
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-07-01 02:35:34 +0000

    Bloxlink temporary API change message

--- slashcommands/bloxlinkcheck.js
+++ slashcommands/bloxlinkcheck.js
@@ -1,6 +1,12 @@
 const bloxlink = require('bloxlink-sdk');
 module.exports = async (interaction) => {
     const commandName = interaction.commandName;
+    interaction
+        .reply({
+            content: `Bloxlink API changed today (6/30/2023) which require's all developer which use bloxklink to create a new global API key, Please wait while the developer's of this bot insert the new global API key (when they get it)`,
+        })
+        .catch(() => {});
+    return;
     await interaction.reply({ content: 'Checking...' }).catch(() => {});
     try {
         const usersofusersxd = interaction.options._hoistedOptions[0].value.replace(/[<@>]/g, '');
