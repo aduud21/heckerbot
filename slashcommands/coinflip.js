f5acfc529ce40f3bb24205b14969db0bd404b36e
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-07-03 22:14:42 +0000

    emoji add

--- slashcommands/coinflip.js
+++ slashcommands/coinflip.js
@@ -9,7 +9,7 @@ module.exports = async (interaction) => {
         interaction
             .reply(
                 `
-${item.question}
+🪙 ${item.question} 🪙
 `
             )
             .catch(() => {});
