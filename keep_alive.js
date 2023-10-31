14ef7022740833e5f8b1f74e188cc0a688cb0d05
aduud21 <74877817+aduud21@users.noreply.github.com>
2023-10-31 02:15:10 +0000

    test

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-const port = 3000;
-app.use(helmet());
-app.listen(port, () => console.log(`✅ -> Keep alive active, listen on port: ${port}`));
+function findAvailablePort() {
+    const port = Math.floor(Math.random() * (9000 - 4000 + 1)) + 4000;
+    return new Promise((resolve, reject) => {
+        const server = app.listen(port, () => {
+            console.log(`✅ -> Keep alive active, listen on port: ${port}`);
+            server.close(() => {
+                resolve(port);
+            });
+        });
+        server.on('error', (error) => {
+            server.close(() => {
+                reject(error);
+            });
+        });
+    });
+}
+findAvailablePort()
+    .then((availablePort) => {
+        app.use(helmet());
+        app.listen(availablePort);
+    })
+    .catch((error) => {
+        console.error(`Failed to find an available port: ${error}`);
+    });
